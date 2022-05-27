d79f6fe0ab34e055198fe2a1ad250c26ad50173d
RafWalker <105276669+RafWalker@users.noreply.github.com>
2022-05-27 14:53:58 +0800

    Script None Test

--- Assets/Script_Test.cs
+++ Assets/Script_Test.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LOL : MonoBehaviour
+public class Script_Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
